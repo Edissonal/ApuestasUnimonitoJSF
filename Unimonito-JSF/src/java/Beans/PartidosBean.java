20f79b85a5d9f928558fe36f70679d8c49ff82f6
edissonal <edissonalonso@gmail.com>
2019-05-26 20:30:11 -0500

    Modulos de partidos

--- Unimonito-JSF/src/java/Beans/PartidosBean.java
+++ Unimonito-JSF/src/java/Beans/PartidosBean.java
@@ -6,14 +6,19 @@
 package Beans;
 
 import Entidades.Apuesta;
+import Entidades.Boleteria;
 import Entidades.Equipo;
+import Entidades.Equipo2;
 import Entidades.Partido;
+import EntidadesEJB.BoleteriaFacadeLocal;
+import EntidadesEJB.Equipo2FacadeLocal;
 import EntidadesEJB.EquipoFacadeLocal;
 import EntidadesEJB.PartidoFacadeLocal;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -24,47 +29,133 @@ import javax.enterprise.context.RequestScoped;
 public class PartidosBean {
 
     @EJB
+    private Equipo2FacadeLocal equipo2Facade;
+
+    @EJB
+    private BoleteriaFacadeLocal boleteriaFacade;
+
+    @EJB
     private EquipoFacadeLocal equipoFacade;
-@EJB
+    @EJB
     private PartidoFacadeLocal partidoFacade;
 
 
 
     private int idpartido;
-    private int idequipo;
-    private int idequipo2;
-    private String NombEquipo;
-    private String NombEquipo2;
     private int marcadorequip1;
     private int marcadorequip2;
-    //private Equipo equipos;
+    private Equipo equipos;
+    private Equipo2 equipos2;
+    private Boleteria boleterias;
   
     
-        public PartidosBean() {
-          //equipos = new Equipo();
-    }
+         public PartidosBean() {
+          equipos = new Equipo();
+          equipos2 = new Equipo2();
+          boleterias = new Boleteria();
+        }
     
         public List<Partido>getPartido(){
     
         return partidoFacade.findAll();
     
     }
-        
-         public List<Equipo>getEquipos(){
-    
-        return equipoFacade.findAll();
-    
-    }  
-
-        
+     
+             
         public String consulta(int id)
     { 
         Partido p =  this.partidoFacade.find(id);       
         this.idpartido = p.getIdPartido();
         this.equipos = p.getIdEquipo();
+        this.equipos2 = p.getIdEquipo2();
+        
+        
         return "definemarca";
     }
 
-   
+       public String crear(){
+        
+       Partido a = new Partido();
+
+       a.setIdEquipo(equipoFacade.find(equipos.getIdEquipo()));
+       a.setIdEquipo2(equipo2Facade.find(equipos2.getIdEquipo2()));
+       a.setMarcadorEquip1(marcadorequip1);
+       a.setMarcadorEquip2(marcadorequip2); 
+       partidoFacade.create(a);
+        FacesContext.getCurrentInstance().getApplication().getNavigationHandler().handleNavigation(FacesContext.getCurrentInstance(), null, "constpartido.xhtml");
+       return "constpartido";
+   }
+
+    public Equipo2FacadeLocal getEquipo2Facade() {
+        return equipo2Facade;
+    }
+
+    public void setEquipo2Facade(Equipo2FacadeLocal equipo2Facade) {
+        this.equipo2Facade = equipo2Facade;
+    }
+
+    public BoleteriaFacadeLocal getBoleteriaFacade() {
+        return boleteriaFacade;
+    }
+
+    public void setBoleteriaFacade(BoleteriaFacadeLocal boleteriaFacade) {
+        this.boleteriaFacade = boleteriaFacade;
+    }
+
+    public EquipoFacadeLocal getEquipoFacade() {
+        return equipoFacade;
+    }
+
+    public void setEquipoFacade(EquipoFacadeLocal equipoFacade) {
+        this.equipoFacade = equipoFacade;
+    }
+
+    public int getIdpartido() {
+        return idpartido;
+    }
+
+    public void setIdpartido(int idpartido) {
+        this.idpartido = idpartido;
+    }
+
+    public int getMarcadorequip1() {
+        return marcadorequip1;
+    }
+
+    public void setMarcadorequip1(int marcadorequip1) {
+        this.marcadorequip1 = marcadorequip1;
+    }
+
+    public int getMarcadorequip2() {
+        return marcadorequip2;
+    }
+
+    public void setMarcadorequip2(int marcadorequip2) {
+        this.marcadorequip2 = marcadorequip2;
+    }
+
+    public Equipo getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(Equipo equipos) {
+        this.equipos = equipos;
+    }
+
+    public Equipo2 getEquipos2() {
+        return equipos2;
+    }
+
+    public void setEquipos2(Equipo2 equipos2) {
+        this.equipos2 = equipos2;
+    }
+
+    public Boleteria getBoleterias() {
+        return boleterias;
+    }
+
+    public void setBoleterias(Boleteria boleterias) {
+        this.boleterias = boleterias;
+    }  
         
 }
